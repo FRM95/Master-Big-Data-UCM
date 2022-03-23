c8c6ce146b80be6c2f1fe0f2770329ccf1c2f93a
Miguel Moreno Mardones <45168574+FRM95@users.noreply.github.com>
2022-03-23 09:50:14 +0100

    Add files via upload

---
+++ Modulo python/saludos.py
@@ -0,0 +1,6 @@
+def saludar():
+    print('Hola, te estoy saludando desde la funcion saludar del modulo saludo')
+    
+class Saludo:
+    def __init__(self):
+        print('Hola, te estoy saludando desde el constructor __init__ de la clase Saludo')
